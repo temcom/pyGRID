915a15b2c41fe0912aa0b9e198f59b7f55646f18
Jacopo Sabbatini <dedalusj@gmail.com>
2013-10-03 06:24:10 +1000

    Added egg files to gitignore

--- setup.py
+++ setup.py
@@ -3,13 +3,11 @@
 from setuptools import setup, find_packages
 
 setup(name="pyGRID",
-      version="0.2",
+      version="0.2.1",
       license="GPLv2",
       keywords="scientific/engineering simulation",
       platforms="OS Independent",
       description="""Python utilities to interface with SUN GRID ENNGINE and allow to run
-                    simulations that span a parameter space""",
-      long_description="""Python utilities to interface with SUN GRID ENNGINE and allow to run
                     simulations that span a parameter space""",
       author="Jacopo Sabbatini",
       author_email="sabbatini@physics.uq.edu.au",
@@ -26,4 +24,15 @@ setup(name="pyGRID",
             'pyGRID = pyGRID:main',
             ],
       },
+      test_suite = 'nose.collector',
+      setup_requires=[
+        'nose>=1.1.2',
+        'mock>=0.8',
+      ],
+      install_requires=[
+        'numpy>=1.6.1',
+      ],
+      extras_require={
+        'docs': 'sphinx',
+      }
       )
